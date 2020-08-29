274c8cb2d5363cc6faadaaaf050c3c4f54d0d9ba
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-08-29 22:33:24 +0700

    Create the counter component

---
+++ my-svelte-project/src/components/Counter.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <button on:click={handleClick}>
+    Clicked {count} {count === 1 ? 'time' : 'times'}
+  </button>
+</main>
